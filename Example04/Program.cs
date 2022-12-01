bcfad4109c5f472292e498f7f1e91ac37d9c7064
Andrei Shvetsov <shved963@gmail.com>
2022-12-02 00:48:51 +0300

    Example04 ready

---
+++ Example04/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 22: Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+int[] array =
+
+5 -> 1, 4, 9, 16, 25. 2 -> 1,4
+*/
+Console.Clear();
+
+Console.WriteLine("Введите число");
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+
+if (!isParsed)
+{
+    Console.WriteLine("Введены не корректные данные");
+    return;
+}
+
+int[] squaresTable = GetNumberSquareTable(num);
+
+PrintArray(squaresTable);
+
+int[] GetNumberSquareTable(int N) // Заполнение массива
+{
+    int[] array = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = (int)(Math.Pow(i+1, 2)); //(int) переделываем double после Pow в int
+    }
+
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
