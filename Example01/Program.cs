d9881842ce7a08f02c7413fe3d9e5a97a8571032
Andrei Shvetsov <shved963@gmail.com>
2022-12-01 22:28:59 +0300

    Example01 ready

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y)
+//причем х != 0 и y != 0 и выдает номер четверти плоскости, в которой находится эта точка.
+
+Console.Clear();
+
+bool isParsedX = int.TryParse(Console.ReadLine(), out int x);
+
+bool isParsedY = int.TryParse(Console.ReadLine(), out int y);
+
+if (!isParsedX || !isParsedY)
+{
+    Console.WriteLine("Координаты введены не корректно");
+    return;
+}
+
+int quarterNumber = GetQuarterNumberByCoord(x,y);
+
+if (quarterNumber == -1)
+{
+    Console.WriteLine("Координаты введены не корректно");
+    return;
+}
+
+Console.WriteLine(quarterNumber);
+
+// возвращаемый_тип_данных НазваниеМетода()
+//{
+//     return 1; оператор return возвращает из метода значение и останавливает его.
+//}
+
+int GetQuarterNumberByCoord(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+
+    if (x > 0 && y < 0)
+    {
+        return 4;
+    }
+
+    return -1;
+}
